e63cbb7749839d0884226b54ea21679ca0c3e991
Reggie Dev <isaacedward2004@gmail.com>
2025-09-05 18:56:54 -0700

    Update austinScraper.js

--- scrapers/austinScraper.js
+++ scrapers/austinScraper.js
@@ -1,1 +1,7 @@
-
+class AustinScraper {
+  async scrapeAndStorePermits() {
+    console.log('--- MOCK SCRAPE: Pretending to scrape Austin permits... ---');
+    return Promise.resolve();
+  }
+}
+module.exports = AustinScraper;
